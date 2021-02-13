5a838cb71f6a8c8ef982b8348c4c0b0e2e1ec968
unknown <LOst>
2021-02-13 20:49:27 +0530

    First commit of all files

---
+++ src/components/TodoList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12"><p class="display-3">Vue Crash Course</p></div>
+    </div>
+    <div class="row">
+      <p>Add create form</p>
+    </div>
+    <div class="row">
+      <div class="col-12 col-lg-6">
+        <ul class="list-group">
+          <Todo
+            v-for="(todo, index) in todos"
+            :key="index"
+            :todoString="todo.todoString"
+            :completed="todo.completed"
+          />
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from "./Todo";
+export default {
+  components: {
+    Todo,
+  },
+  data() {
+    return {
+      todos: [
+        { todoString: "Make Angular Course", completed: false },
+        { todoString: "Make React Course", completed: false },
+        { todoString: "Make Flutter Course", completed: true },
+        { todoString: "Make ML Course", completed: true },
+      ],
+    };
+  },
+  methods: {
+    addTodo(newTodo) {
+      this.todos.push({ todoString: newTodo, completed: false });
+    },
+    toggleTodo(todo) {
+      todo.completed = !todo.completed;
+    },
+    editTodo(todo, newTodoString) {
+      todo.todoString = newTodoString;
+    },
+    deleteTodo(deleteTodo) {
+      this.todos = this.todos.filter(
+        (todo) => todo.todoString !== deleteTodo.todoString
+      );
+    },
+  },
+};
+</script>
+
+<style>
+</style>
